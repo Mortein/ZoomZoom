e65efa42de8a7922a50e2795ed716a21457d4d3b
Kiel Hurley <kielhurley@gmail.com>
2020-05-11 20:07:29 +1200

    Add examples

--- ZoomZoom/Public/Start-ZoomMeeting.ps1
+++ ZoomZoom/Public/Start-ZoomMeeting.ps1
@@ -9,11 +9,28 @@ ID of meeting to join, full meeting URL, or full meeting URL with password
 Password to join meeting, if not included in MeetingID
 
 .EXAMPLE
-Start-ZoomMeeting
+Start-ZoomMeeting -MeetingID "123-4444-5555"
+Connect to meeting using only meeting ID
+
+.EXAMPLE
+Start-ZoomMeeting -MeetingID "123-4444-5555" -Password "422562"
+Connect to meeting using meeting ID and password
+
+.EXAMPLE
+Start-ZoomMeeting -MeetingID 99922224444
+Connect to meeting using only meeting ID
+
+.EXAMPLE
+Start-ZoomMeeting -MeetingID "https://zoom.us/j/77712348888"
+Connect to meeting using URL
+
+.EXAMPLE
+Start-ZoomMeeting -MeetingID "https://zoom.us/j/88833335555?pwd=NE0vT2RkV25SWnBxMC9RRXFUc1F2dz09"
+Connect to meeting using URL with password included
 #>
 function Start-ZoomMeeting {
     param(
-        [Parameter(Mandatory = $true)] [string] $MeetingID,
+        [Parameter(Mandatory = $true, Position = 0)] [string] $MeetingID,
         [String] $Password
     )
 
