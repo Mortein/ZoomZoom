e5285b2c096f23050feb95b40fa4d3f1c7f99d71
Kiel Hurley <kielhurley@gmail.com>
2020-05-11 21:34:54 +1200

    Test and Publish

---
+++ Publish-ZoomZoom.ps1
@@ -0,0 +1,5 @@
+param (
+    [string] $NuGetApiKey
+)
+
+Publish-Module -Path ZoomZoom -NuGetApiKey $NuGetApiKey -Verbose
---
+++ Test-ZoomZoom.ps1
@@ -0,0 +1,18 @@
+param (
+    [string] $NuGetApiKey
+)
+
+# Check for required modules
+"PSScriptAnalyzer" | ForEach-Object {
+    $Module = Get-Module -Name "$_" -ListAvailable
+    if (!$Module) {
+        Write-Error -Message "Please install module $_"
+        break
+    }
+}
+
+Test-ModuleManifest -Path (Join-Path -Path $PSScriptRoot -ChildPath ZoomZoom | Join-Path -ChildPath "ZoomZoom.psd1")
+
+Invoke-ScriptAnalyzer -Path ZoomZoom -ReportSummary -Recurse
+
+Publish-Module -Path ZoomZoom -NuGetApiKey $NuGetApiKey -WhatIf -Verbose
