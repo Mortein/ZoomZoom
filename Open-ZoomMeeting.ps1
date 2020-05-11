72f3479cfab9416016d735cc0b08e8552b0353aa
Kiel Hurley <kielhurley@gmail.com>
2020-05-11 17:03:18 +1200

    Split a URL
    Supply either the full URL, or just the MeetingID for MeetingID

--- Open-ZoomMeeting.ps1
+++ Open-ZoomMeeting.ps1
@@ -3,4 +3,20 @@ param(
     [String] $Password
 )
 
+# Extract Password from URL
+if (!$Password) {
+    $Parts = $MeetingID -split "pwd="
+
+    if ($Parts.Count -gt 1) {
+        # There's a password included, use it
+        $Password = $Parts[1]
+    }
+}
+
+# Get the MeetingID, if they've supplied a URL
+$MeetingID = $MeetingID -replace "\?pwd=$Password"
+$MeetingIDParts = $MeetingID -split "/"
+$MeetingID = $MeetingIDParts[$MeetingIDParts.Count - 1]
+$MeetingID = $MeetingID -replace "-"
+
 Start-Process -FilePath "zoommtg://zoom.us/join?action=join&confno=$MeetingID&pwd=$Password"
