1d84bc8071343250bf6c38ff0bb4cd09e8448a1b
Kiel Hurley <kielhurley@gmail.com>
2020-05-12 22:09:39 +1200

    Update manifest on publish
    Increment version build by 1 from PSGallery
    Update functions using Public scripts

--- Publish-ZoomZoom.ps1
+++ Publish-ZoomZoom.ps1
@@ -2,4 +2,20 @@ param (
     [Parameter(Mandatory = $true)] [string] $NuGetApiKey
 )
 
+$ModuleName = "ZoomZoom"
+
+# Find out what the version should be
+[version] $Version = "1.0.0"
+$Module = Find-Module -Name "$ModuleName" -ErrorAction SilentlyContinue
+if ($Module) {
+    $Version = ("{0}.{1}.{2}" -f $Version.Major, $Version.Minor, $Version.Build + 1)
+}
+
+# Get the list of module functions
+$Functions = @()
+$Functions += Get-ChildItem -Path (Join-Path -Path "$ModuleName" -ChildPath "Public") -File | ForEach-Object { $_.BaseName }
+
+# Update the manifest
+Update-ModuleManifest -Path (Join-Path -Path "$ModuleName" -ChildPath "$ModuleName.psd1") -ModuleVersion $Version -FunctionsToExport $Functions
+
 Publish-Module -Path ZoomZoom -NuGetApiKey $NuGetApiKey -Verbose
