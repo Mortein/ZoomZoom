f23b4dd81827435393a706662743c4874f2a85b6
Kiel Hurley <kielhurley@gmail.com>
2020-05-12 23:10:22 +1200

    Prettier tests
    Return non-zero on errors, everything else is fine for now

--- Test-ZoomZoom.ps1
+++ Test-ZoomZoom.ps1
@@ -4,6 +4,8 @@ param (
 
 $ModuleName = "ZoomZoom"
 
+$Errors = 0
+
 # Check for required modules
 "PSScriptAnalyzer" | ForEach-Object {
     $Module = Get-Module -Name "$_" -ListAvailable
@@ -12,10 +14,38 @@ $ModuleName = "ZoomZoom"
     }
 }
 
-Test-ModuleManifest -Path (Join-Path -Path $PSScriptRoot -ChildPath "$ModuleName" | Join-Path -ChildPath "$ModuleName.psd1")
-
-Invoke-ScriptAnalyzer -Path "$ModuleName" -ReportSummary -Recurse
-
-if ($null -eq $NuGetApiKey) {
-    Publish-Module -Path $ModuleName -NuGetApiKey $NuGetApiKey -WhatIf -Verbose
+# Module manifest
+try {
+    Test-ModuleManifest -Path (Join-Path -Path $PSScriptRoot -ChildPath "$ModuleName" | Join-Path -ChildPath "$ModuleName.psd1") -ErrorAction Stop | Out-Null
 }
+catch {
+    Write-Error $_
+    $Errors += 1
+}
+
+# Script analyzer
+$AnalyzerResults = Invoke-ScriptAnalyzer -Path "$ModuleName" -Severity 'Information', 'Warning', 'Error' -Recurse
+$AnalyzerResults | ForEach-Object {
+    [PSCustomObject]@{
+        RuleName   = $_.RuleName
+        ScriptName = $_.ScriptName
+        Line       = $_.Line
+    }
+
+    if ($_.Severity -in 'Error') {
+        $Errors += 1
+    }
+} | Sort-Object -Property 'ScriptName', 'Line'
+
+# Publish
+if ($null -eq $NuGetApiKey) {
+    try {
+        Publish-Module -Path "$ModuleName" -NuGetApiKey $NuGetApiKey -WhatIf -Verbose -ErrorAction Stop
+    }
+    catch {
+        Write-Error $_
+        $Errors += 1
+    }
+}
+
+exit $Errors
