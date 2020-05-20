a69c990da70ab7f81797f5677420355f22e3cdbe
Richard Brownlee <rabrownlee@hotmail.com>
2020-05-20 23:36:58 +1200

    Calculate the weekly recurring meetings

--- ZoomZoom/Private/Get-RecurringMeetingDates.ps1
+++ ZoomZoom/Private/Get-RecurringMeetingDates.ps1
@@ -13,39 +13,67 @@ function Get-RecurringMeetingDates {
         # An Outlook appointment item
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [object]
-        $Appointment,
-
-        # A DateArray from Get-DaysInRange that contains datetime objects
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [array]
-        $DateArray
+        $Appointment
     )
 
+    $DateArray = Get-DaysinRange -StartDate $Appointment.Start -EndDate Get-Date
+    $Results = @()
     Switch (($Appointment.GetRecurrencePattern()).DayOfWeekMask) {
         #CdoMonday/2 - The appointment recurs on Mondays.
         2 {
-            Return $DateArray | Where-Object -Property DayOfWeek -eq 'Monday'
+            $DayOfWeekMask = $DateArray | Where-Object -Property DayOfWeek -eq 'Monday'
         }
         #CdoTuesday/4 - The appointment recurs on Tuesdays.
         4 {
-            Return $DateArray | Where-Object -Property DayOfWeek -eq 'Tuesday'
+            $DayOfWeekMask = $DateArray | Where-Object -Property DayOfWeek -eq 'Tuesday'
         }
         #CdoWednesday/8 - The appointment recurs on Wednesdays.
         8 {
-            Return $DateArray | Where-Object -Property DayOfWeek -eq 'Wednesday'
+            $DayOfWeekMask = $DateArray | Where-Object -Property DayOfWeek -eq 'Wednesday'
         }
         #CdoThursday/16 - The appointment recurs on Thursdays.
         16 {
-            Return $DateArray | Where-Object -Property DayOfWeek -eq 'Thursday'
+            $DayOfWeekMask = $DateArray | Where-Object -Property DayOfWeek -eq 'Thursday'
         }
         #CdoFriday/32 - The appointment recurs on Fridays.
         32 {
-            Return $DateArray | Where-Object -Property DayOfWeek -eq 'Friday'
+            $DayOfWeekMask = $DateArray | Where-Object -Property DayOfWeek -eq 'Friday'
         }
         #Meeting that occur on every week day have the value of 62 (2+4+8+16+32)
-        62 {
+        #62 {
+        #    $DayOfWeekMask += $DateArray | Where-Object -Property DayOfWeek -NotMatch ^[S*]
+        #    return $Results
+        #}
+    }
+
+    Switch (($Appointment.GetRecurrencePattern()).RecurrenceType) {
+        #CdoRecurTypeDaily/0 - Appointments that recur daily
+        0 {
             $Results += $DateArray | Where-Object -Property DayOfWeek -NotMatch ^[S*]
             return $Results
         }
+        #CdoRecurTypeWeekly/1 - Appointment recurs weekly (DayOfWeekMask,Interval)
+        1 {
+            for ($i = 0; $i -lt ($DayOfWeekMask).Count; $i += (($Appointment.GetRecurrencePattern()).Interval)) {
+                $Results += $DayOfWeekMask[$i]
+            }
+        }
+        #CdoRecurTypeMonthly/2 - Appointment recurs monthly
+        2 {
+
+        }
+        #CdoRecurTypeMonthlyNth/3 - Appointment recurs every Nth month
+        3 {
+
+        }
+        #CdoRecurTypeYearly/5 - Appointment recurs every year
+        5 {
+
+        }
+        #CdoRecurTypeYearlyNth/6 - Appointment recurs every Nth year
+        6 {
+
+        }
+
     }
 }
