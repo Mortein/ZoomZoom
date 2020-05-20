b0e405cf89e5a12a9d9de1600786cea60054fb35
Richard Brownlee <rabrownlee@hotmail.com>
2020-05-20 23:58:02 +1200

    Corrected variable names

--- ZoomZoom/Private/Get-DaysInRange.ps1
+++ ZoomZoom/Private/Get-DaysInRange.ps1
@@ -34,8 +34,8 @@ function Get-DaysInRange {
     }
 
     process {
-        $DateArray += $Start
-        $WorkingTime = $Start
+        $DateArray += $StartDate
+        $WorkingTime = $StartDate
         foreach ($_ in 1..$Timespan.Days) {
             $WorkingTime = $WorkingTime.AddDays(1)
             $DateArray += $WorkingTime
