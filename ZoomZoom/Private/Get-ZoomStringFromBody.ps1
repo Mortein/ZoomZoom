d15e7752499b34743d145a1b6f2be1d2ce136a65
Richard Brownlee <rabrownlee@hotmail.com>
2020-05-21 21:14:25 +1200

    Match a zoom URL from a string of text

---
+++ ZoomZoom/Private/Get-ZoomStringFromBody.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+    Searches a string for a Zoom Meeting invite URL and returns the first instance found
+.DESCRIPTION
+    Searches a string of text for any Zoom URL's based off the standard Zoom uses. Returns the first line found and splits this incase
+    there are multiple URLS in 1 line (Common for hyperlink formats)
+.INPUTS
+    String
+.OUTPUTS
+    String
+.NOTES
+    Used to scrape meeting object bodies for Zoom meeting URLs
+#>
+function Get-ZoomStringFromBody {
+    [CmdletBinding()]
+    param (
+        # A body of text that will contain a Zoom URL
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        $MeetingBody
+    )
+
+    begin {
+    }
+
+    process {
+        $SplitString = $MeetingBody -Split ([Environment]::NewLine)
+        Foreach ($String in $SplitString) {
+            if ($String -match "\D{5,6}\/\/\S*\/j\/\d{10,11}\?{1}\S*") {
+                $Results = ((Select-String -InputObject $String -Pattern "\D{5,6}\/\/\S*\/j\/\d{10,11}\?{1}\S*") -split " ")[0]
+            }
+            elseif ($String -match "\D{5,6}\/\/\S*\/j\/\d{10,11}") {
+                $results = ((Select-String -InputObject $String -Pattern "\D{5,6}\/\/\S*\/j\/\d{10,11}") -split " ")[0]
+            }
+        }
+    }
+
+    end {
+        return $results
+    }
+}
